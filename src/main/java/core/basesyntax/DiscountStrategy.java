e2b9f58ffe065f225a2afd19522920b6e75a6c78
Eugene L <lugovoj20@gmail.com>
2026-04-28 21:19:16 +0200

    fixed checkstyle warnings

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -5,7 +5,6 @@ import core.basesyntax.strategy.impl.BirthdayDiscountService;
 import core.basesyntax.strategy.impl.BlackFridayDiscountService;
 import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
-
 import java.util.HashMap;
 import java.util.Map;
 
